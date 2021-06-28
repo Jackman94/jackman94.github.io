2b919e6bcf7283e55b273e622f64bcc099af49f6
jackman <apels1994@gmail.com>
2021-06-29 00:28:28 +0300

    redesign

--- OleksiiTiutiunnyk/js/swiper-config.js
+++ OleksiiTiutiunnyk/js/swiper-config.js
@@ -5,15 +5,23 @@ window.addEventListener('load', function () {
         slidesPerView: 1,
         navigation: {
             nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
+            prevEl: ".swiper-button-prev"
+        }
     });
     new Swiper(".shop-container-swiper", {
         spaceBetween: 30,
         slidesPerView: 3,
         navigation: {
             nextEl: ".shop-next-button",
-            prevEl: ".shop-prev-button",
-        },
+            prevEl: ".shop-prev-button"
+        }
     });
-})
+    new Swiper(".album-container-slider", {
+        spaceBetween: 30,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: ".album-next-button",
+            prevEl: ".album-prev-button"
+        }
+    });
+});
